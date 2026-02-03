358b3a6e56a75c0515868ba75089dda20f59b94b
Eduardor7624 <edu7624@hotmail.com>
2026-02-03 15:00:47 -0500

    ave

--- DbConnections.cs
+++ DbConnections.cs
@@ -8,13 +8,10 @@ namespace AmerciaMarketSecFactWeb
 {
     public static class DbConnections
     {
-        public const string AmericaMarket =
-            "Data Source=AM1\\AMDEV;Initial Catalog=AmericaMarket;User ID=sa;Password=Rewq1234;TrustServerCertificate=True";
+        //public const string AmericaMarket =  "Data Source=AM1\\AMDEV;Initial Catalog=AmericaMarket;User ID=sa;Password=Rewq1234;TrustServerCertificate=True";
 
-        public const string AmericaMarketSec =
-            "Data Source=AM1\\AMDEV;Initial Catalog=AmericaMarketSec;User ID=sa;Password=Rewq1234;TrustServerCertificate=True";
+        public const string AmericaMarketSec = "Data Source=AM1\\AMDEV;Initial Catalog=AmericaMarketSec;User ID=sa;Password=Rewq1234;TrustServerCertificate=True";
 
-        public const string AmericaMarketCommon =
-            "Data Source=AM1\\AMDEV;Initial Catalog=AmericaMarketCommon;User ID=sa;Password=Rewq1234;TrustServerCertificate=True";
+        //public const string AmericaMarketCommon = "Data Source=AM1\\AMDEV;Initial Catalog=AmericaMarketCommon;User ID=sa;Password=Rewq1234;TrustServerCertificate=True";
     }
 }
